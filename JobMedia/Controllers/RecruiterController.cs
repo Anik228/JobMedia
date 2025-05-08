ae97bef927107c1504b5f4ff81c1193a2f5c6869
Anik Lal Dey <anikdey228@gmail.com>
2025-05-08 08:54:01 +0600

    updated

--- JobMedia/Controllers/RecruiterController.cs
+++ JobMedia/Controllers/RecruiterController.cs
@@ -53,5 +53,50 @@ namespace JobMedia.Controllers
 
             return PartialView(profile);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateProfile(UpdateProfile updatedUser)
+        {
+            if (!ModelState.IsValid)
+            {
+               
+                foreach (var key in ModelState.Keys)
+                {
+                    var error = ModelState[key].Errors.FirstOrDefault();
+                    if (error != null)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Error in {key}: {error.ErrorMessage}");
+                    }
+                }
+                // return View("Profile", updatedUser);
+                return RedirectToAction("Recruiter", "Recruiter");
+            }
+
+            var existingUser = db.User.FirstOrDefault(u => u.Email == updatedUser.Email);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            existingUser.Name = updatedUser.Name;
+            //existingUser.Gender = updatedUser.Gender;          
+
+            int affectedRows = db.SaveChanges();
+
+            if (affectedRows > 0)
+            {
+                TempData["Success"] = "Profile updated successfully!";
+            }
+            else
+            {
+                TempData["Error"] = "Failed to update profile. Please try again.";
+            }
+
+
+            // return PartialView(profile);
+
+            return RedirectToAction("Recruiter", "Recruiter");
+        }
     }
 }
--- JobMedia/Models/UpdateProfile.cs
+++ JobMedia/Models/UpdateProfile.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace JobMedia.Models
 {
-    public class LoginDto
+    public class UpdateProfile
     {
+       public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; }
+     
     }
 }
